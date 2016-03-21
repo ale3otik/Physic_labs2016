0399da85753a9a544dca037516ee773eb60fef49
Alex <aleksey.zotov@phystech.edu>
2016-03-21 20:43:41 +0300

    some changes

---
+++ Laba2/graph.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from math import sqrt
+a = np.arange(4, 52, 4)
+tlool = [53.87 , 43.19 , 36.68 , 33.28 , 31.69 , 30.90 , 30.57 , 30.68 , 31.13 , 31.31 , 31.85 , 32.69]
+T = [x / 20 for x in tlool]
+# plt.title("T(a)")
+# plt.plot(a,T,'.-r')
+# plt.show()
+
+T2a = [T[i]**2 * a[i]/100.0 for i in range(len(a))]
+a2 = [x**2 / 10000.0 for x in a]
+plt.plot(a2 ,T2a , 'o--g')
+for i in range(len(a)):
+    print (str(a2[i]) + " " + str(T2a[i]))
+
+
+b = 0;
+sx = 0
+sy = 0
+sx2 = 0
+n = len(a2)
+for i in a2:
+    sx += i
+    sx2 += i**2
+xcp = sx / n
+
+for i in T2a:
+    sy += i
+ycp = sy / n
+
+
+smult = 0;
+for i in range(n):
+    smult += a2[i] * T2a[i]
+k = (n * smult - sx * sy) / (n * sx2 - sx**2)
+b = ycp - xcp * k
+I2 = [b + k*x for x in a2]
+plt.plot(a2, I2, '-r')
+plt.grid(True , linewidth=0.3);
+plt.title("k = " + str(k)  + " " + "b = " + str(b))
+plt.show()
+print("lool")
+#print(1 / k)
+#print((1/k) * 4 * (3.14**2))
+print (sqrt((b / k) * 12))
---
+++ Laba2/laba.py
@@ -0,0 +1,33 @@
+from math import *
+x = [31.43 , 31.72, 31.56 , 31.62 , 31.49 , 31.53]
+# ln = ""
+# for v in x:
+#     ln += "&" + str(v)
+# print(ln)
+# x = [31.56 , 31.34 , 31.6 , 31.62 , 31.63 , 31.54]
+
+s = 0
+for i in x:
+    s = s + i
+Tcp = s/ (6 * 20)
+tcp = s / 6;
+xp = [p - tcp for p in x]
+
+
+sigm = 0
+for p in xp:
+    sigm += p**2
+sigm = sqrt(sigm / 5)
+
+print(Tcp)
+print (sigm)
+eps = sigm / (20 * Tcp)
+print(eps)
+# # print(sigm / (0.005 * Tcp))
+# # print(10/7.5)
+# print((5 * 60 + 30) / Tcp)
+
+
+# print(31.40  / 20)
+# Q = (pi * 330) / (Tcp * log(1.3))
+# print(Q)
