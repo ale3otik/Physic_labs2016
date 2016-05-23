b3176808d13085e01d16c5c1270576563f8240e1
Alex <aleksey.zotov@phystech.edu>
2016-05-23 19:15:16 +0300

    string completed

--- Laba5/help.py
+++ Laba5/help.py
@@ -1,5 +1,17 @@
-X = [0 , 208.7, 418.3 , 629.0 , 837.1]
-s = ""
-for x in X :
-    s += '&' + str(x) 
-print s
+import numpy as np
+import matplotlib.pyplot as plt
+from math import *
+
+# X = [0 , 208.7, 418.3 , 629.0 , 837.1]
+# s = ""
+# for x in X :
+#     s += '&' + str(x) 
+# print s
+
+u = np.array([99.0,129.4 ,166.8, 172.0,209.5])
+m = np.array([548.0,1042.3,1544.1, 2009.0, 2514.5])
+print(2 * 129.4 * 0.5)
+m = m / 1e3 
+F = m * 9.81
+print(u**2)
+print(F)
--- Laba5/mnk_d.py
+++ Laba5/mnk_d.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import *
 
-X = np.array([0,1,2,3,4])
-Y = np.array([0 , 208.7, 418.3 , 629.0 , 837.1])
+X = np.array([  5.37588  , 10.224963 , 15.147621 ,  19.70829  , 24.667245])
+Y = np.array([  9801.  ,  16744.36  , 27822.24  , 29584.  ,  43890.25])
 th = np.polyfit(X,Y,1)
 k = th[0]
 b = th[1]
@@ -18,14 +18,25 @@ print(sb)
 
 OY = [k * x + b for x in X]
 
+DY = [[ y - 129.4 , y + 129.4] for y in Y]
 
 kr =  100
-plt.figure(figsize=(15,7))
-plt.grid(True)
-# plt.scatter(X, Y, s=100 ,marker='+', color='g')
-plt.plot(X,OY,'--r')
-plt.plot(X,Y,'-ob' , alpha=0.5)
-# plt.xlim([-0.1,len(X)-1 + 0.1])
-# plt.ylim([-10,Y[len(X)-1] + 100])
-plt.savefig('m4.png') 
-plt.show()
+# plt.figure(figsize=(15,7))
+# plt.grid(True)
+# plt.title('$u^2(F)$')
+# plt.xlabel(r'$F$,H')
+# plt.ylabel(r'$u^2$ ,$ (m/s)^2$')
+# # plt.scatter(X, Y, s=129.4 ,marker='+', color='g')
+# plt.plot(X,OY,'--r')
+# plt.plot(X,Y,'-b' , alpha=0.5)
+# for i in range(len(Y)) :
+#     plt.plot([X[i],X[i]], DY[i], color='g')
+# plt.xlim([5,25])
+# plt.ylim([7000,45000])
+# plt.savefig('u2.png') 
+# plt.show()
+
+ro = 1 / k
+sro = 1 / k**2 * sk
+print(ro * 10 ** 3)
+print(sro * 10 ** 3)
